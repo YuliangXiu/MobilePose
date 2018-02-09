784f2351ebb8f24fedb393abe251f79144a710c7
yuliangxiu <yuliangxiu@gmail.com>
2018-02-09 22:18:06 +0800

    webcam

---
+++ gpu2cpu.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        model = models.resnet18(pretrained=True)
+        for param in model.parameters():
+            param.requires_grad = True
+        model.fc=nn.Linear(512,32)
+        self.resnet = model
+
+    def forward(self, x):
+        pose_out = self.resnet(x)
+        return pose_out
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+# os.environ["CUDA_VISIBLE_DEVICES"]=""
+# os.environ["CUDA_LAUNCH_BLOCKING"]="1"
+# torch.cuda.set_device(1) 
+torch.backends.cudnn.enabled = True
+print(torch.cuda.device_count())
+gpus = [0,1]
+
+
+checkpoint_path = "./checkpoint20.t7"
+net = torch.load(checkpoint_path, map_location={'cuda:1' : 'cuda:1', 'cuda:0': 'cuda:0'}).cuda(device_id=gpus[0])
+# net = torch.load(checkpoint_path)
+net_cpu = net.cpu()
+torch.save(net_cpu, "./checkpoint-cpu-20.t7")
+torch.save(net_cpu.state_dict(), "./checkpoint-cpu-20-state.t7")

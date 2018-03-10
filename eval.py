465cbcdee77e76a0619979711f31c7460d86e716
yuliangxiu <yuliangxiu@gmail.com>
2018-03-10 19:58:00 +0800

    add FPS

--- eval.py
+++ eval.py
@@ -26,6 +26,7 @@ import torch
 import csv
 import os
 import argparse
+import time
 
 from dataloader import *
 from coco_utils import *
@@ -47,13 +48,12 @@ if __name__ == '__main__':
 
     # user defined parameters
     filename = "final-aug.t7"
-    test_dataset_size = 200
     num_threads = 10
 
     PATH_PREFIX = "/home/yuliang/code/DeepPose-pytorch/results/{}".format(modeltype)
     full_name="/home/yuliang/code/DeepPose-pytorch/models/{}/{}".format(modeltype, filename)
-    # full_name = "/home/yuliang/code/DeepPose-pytorch/models/demo/mobilenetv2_224x224.t7"
-    # full_name = "/home/yuliang/code/DeepPose-pytorch/models/demo/resnet18_227x227-robust.t7"
+    # full_name = "/home/yuliang/code/DeepPose-pytorch/models/demo/mobilenetv2_224x224-robust.t7"
+    full_name = "/home/yuliang/code/DeepPose-pytorch/models/demo/resnet18_227x227.t7"
     ROOT_DIR = "/home/yuliang/code/deeppose_tf/datasets/mpii"
     
     if modeltype == 'resnet':
@@ -71,8 +71,11 @@ if __name__ == '__main__':
                                             Expansion(),
                                             ToTensor()
                                         ]))
+    test_dataset_size = len(test_dataset)
+
     test_dataloader = DataLoader(test_dataset, batch_size=test_dataset_size,
                             shuffle=False, num_workers = num_threads)
+
     # get all test data
     all_test_data = {}
     for i_batch, sample_batched in enumerate(tqdm(test_dataloader)):
@@ -84,9 +87,13 @@ if __name__ == '__main__':
         eval_coco('/home/yuliang/code/PoseFlow/checkpoint140.t7', 
         'result-gt-json.txt', 'result-pred-json.txt')
         """
-        # load net
+        # gpu mode
         net = Net().cuda(device_id=gpus[0])
         net = torch.load(net_path).cuda(device_id=gpus[0])
+
+        # cpu mode
+        # net = Net()
+        # net = torch.load(net_path, map_location=lambda storage, loc: storage)
 
         ## generate groundtruth json
         total_size = len(all_test_data['image'])
@@ -107,8 +114,17 @@ if __name__ == '__main__':
         all_coco_pred_annotations_arr = [] 
         for i in tqdm(range(1, int(ceil(total_size / 100.0 + 1)))):
             sample_data = {}
+
+            # gpu mode
             sample_data['image'] = all_test_data['image'][100 * (i - 1) : min(100 * i, total_size)].cuda(device=gpus[0])
+            # cpu mode
+            # sample_data['image'] = all_test_data['image'][100 * (i - 1) : min(100 * i, total_size)]
+
+            # print('test dataset contains: %d'%(len(sample_data['image'])))
+            # t0 = time.time()
             output = net(Variable(sample_data['image'],volatile=True))
+            # print('FPS is %f'%(1.0/((time.time()-t0)/len(sample_data['image']))))
+
             transform_to_coco_pred(output, all_coco_pred_annotations_arr, 100 * (i - 1))
 
         all_coco_pred_annotations_arr = [item._asdict() for item in all_coco_pred_annotations_arr]
